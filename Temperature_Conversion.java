e2de2ab95884e9ba5983830f9ea610285485508f
mahithahanu <119840694+mahithahanu@users.noreply.github.com>
2024-02-01 11:45:26 +0530

    import java.util.*; public class Degree{     public static void main(String args[]){         Scanner sc=new Scanner(System.in);         double n=sc.nextDouble();         double f= 32+((n*9)/5);         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Degree{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        double n=sc.nextDouble();
+        double f= 32+((n*9)/5);
+        System.out.printf("%.2f",f);
+    }
+}
